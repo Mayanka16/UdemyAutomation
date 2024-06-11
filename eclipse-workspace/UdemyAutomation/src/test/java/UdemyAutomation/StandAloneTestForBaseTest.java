d0833fe0960ddca761ec4cdc85e4863b57139b75
Mayanka Sao <mayankasao@gmail.com>
2024-06-11 17:48:34 +0530

    Added Jackson Databind logic to read Json File

--- eclipse-workspace/UdemyAutomation/src/test/java/UdemyAutomation/StandAloneTestForBaseTest.java
+++ eclipse-workspace/UdemyAutomation/src/test/java/UdemyAutomation/StandAloneTestForBaseTest.java
@@ -1,6 +1,8 @@
 package UdemyAutomation;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 import org.testng.Assert;
 import org.testng.AssertJUnit;
@@ -19,18 +21,19 @@ public class StandAloneTestForBaseTest extends BaseTest {
 
 	@Test(dataProvider = "getData", groups = { "Purchase", "Sanity" })
 
-	public void purchaseCloth(String email, String password, String productName) throws IOException {
+	//public void purchaseCloth(String email, String password, String productName) throws IOException {
 
-		landingPageObj.loginApplication(email, password);
+	public void purchaseCloth(HashMap<String, String> input) throws IOException {
+		landingPageObj.loginApplication(input.get("email"), input.get("password"));
 
 		ProductCatalog productCatalogObj = new ProductCatalog(driver);
 
 		productCatalogObj.getProductList();
-		productCatalogObj.addProductToCart(productName);
+		productCatalogObj.addProductToCart(input.get("productName"));
 		productCatalogObj.goToCartPage();
 
 		MyCart myCartobj = new MyCart(driver);
-		Boolean match = myCartobj.verifyProductDisplay(productName);
+		Boolean match = myCartobj.verifyProductDisplay(input.get("productName"));
 		Assert.assertTrue(match);
 		myCartobj.checkOut();
 
@@ -58,9 +61,25 @@ public class StandAloneTestForBaseTest extends BaseTest {
 	}
 
 	@DataProvider
-	public Object[][] getData() {
-		return new Object[][] { { "maya@gmail.com", "Quality44%", "ZARA COAT 3" },
-				{ "sao@gmail.com", "Quality44%", "ADIDAS ORIGINAL" } };
+	public Object[][] getData() throws IOException {
+		/*
+		 * converted to Json file reader
+		 * 
+		 * 
+		 * HashMap<String, String> map = new HashMap<>(); map.put("email",
+		 * "maya@gmail.com"); map.put("password", "Quality44%"); map.put("productName",
+		 * "ZARA COAT 3");
+		 * 
+		 * HashMap<String, String> map1 = new HashMap<>(); map1.put("email",
+		 * "sao@gmail.com"); map1.put("password", "Quality44%"); map1.put("productName",
+		 * "ADIDAS ORIGINAL");
+		 * 
+		 */
+		
+		//return new Object[][] { { "maya@gmail.com", "Quality44%", "ZARA COAT 3" },{ "sao@gmail.com", "Quality44%", "ADIDAS ORIGINAL" } };
+		List<HashMap<String, String>> data = getJsonDataToHashMap(System.getProperty("user.dir") + "//src//test//java//jsonData//PurchaseCloth.json");
+		return new Object[][] { {data.get(0)},{data.get(1)}};
+				
 	}
 
 }
--- eclipse-workspace/UdemyAutomation/src/test/java/com/qa/TestComponents/BaseTest.java
+++ eclipse-workspace/UdemyAutomation/src/test/java/com/qa/TestComponents/BaseTest.java
@@ -1,15 +1,22 @@
 package com.qa.TestComponents;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.pageObjects.LandingPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -43,6 +50,23 @@ public class BaseTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		return driver;
 	}
+	
+	public List<HashMap<String, String>> getJsonDataToHashMap(String filePath) throws IOException {
+		// reading json to string
+		// FileUtils.readFileToString(new
+		// File("//Users//mayanka//eclipse-workspace//UdemyAutomation//src//test//java//jsonData//PurchaseCloth.json"));
+		String jsonContent = FileUtils.readFileToString(
+				new File(filePath), StandardCharsets.UTF_8);
+		
+		//String to HashMap using Jackson Databind
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>(){
+			
+			
+		});
+		return data;
+	}
+	
 
 	@BeforeMethod(alwaysRun = true) //(alwaysRun = true) in any case this method will always run
 
